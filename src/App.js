3bed674eda8dd1731f0fccff5848bc373b7bd6d1
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-02-12 08:55:26 +0700

    Dynamic Content

--- src/App.js
+++ src/App.js
@@ -7,20 +7,31 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Taufan'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 1
+        },
+        {
+          name: 'Buto Ijo',
+          id: 2
+        },
+        {
+          name: 'Genderuwo',
+          id: 3
+        },
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello Evita' })}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={ monster.id }>{ monster.name }</h1>
+          ))
+        }
       </div>
     );
   }
