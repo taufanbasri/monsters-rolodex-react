e97857f1fb9a0cfcfc9bf0343a2218d4bcd1e844
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-02-12 13:41:06 +0700

    Card Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,15 @@
+import Card from '../card/card.component';
 import './card-list.styles.css';
 
 const CardList = (props) => {
     return (
-        <div className="card-list">{ props.children }</div>
+        <div className="card-list">
+            {
+                props.monsters.map(monster => (
+                    <Card key={ monster.id } monster={ monster } />
+                ))
+            }
+        </div>
     );
 }
  
